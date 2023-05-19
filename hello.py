ab8be792db59fe5432bb86f83d04cd314ff6ab9d
priya4565 <93039352+priya4565@users.noreply.github.com>
2023-05-19 22:33:49 +0530

    pushing the files

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name
+
+print('hello world')
+print('What is your name?')
+myname=input()
+print('good meeting you, '+myname)
+print('length of your name is ')
+print(len(myname))
+print('age? ')
+myage=input()
+print('you will be '+str(int(myage)+1)+ ' in a year')
